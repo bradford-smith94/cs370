d464581b9527ece6967eda7eff71ea56a808fbb9
Bradford Smith <bsmith8@stevens.edu>
2016-02-01 20:36:00 -0500

    Started ProjectEuler
    Largest Prime Factor... didn't have time to start

---
+++ projectEuler/largestPrimeFactor/lpf.c
@@ -0,0 +1,13 @@
+/* Bradford Smith (bsmith8)
+ * Project Euler LargestPrimeFactor lpf.c
+ * 02/01/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
