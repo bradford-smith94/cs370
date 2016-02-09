220bc52f9cf7fe62403031878c2a8857f4308ec5
Bradford Smith <bsmith8@stevens.edu>
2016-02-08 20:38:07 -0500

    Assignment2: SPOJ ONP
    Initial skeleton structure.

---
+++ spoj/onp/onp.c
@@ -0,0 +1,26 @@
+/* Aidan Racaniello, Bradford Smith, Nicholas Zubrycki
+ * CS 370 Assignment 2: SPOJ ONP onp.c
+ * 02/08/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numExpr; /* number of expressions to read */
+    char expr[401]; /* the expression */
+    int i;
+
+
+    /* read in number of expressions */
+    scanf("%d\n", &numExpr);
+
+    for (i = 0; i < numExpr; i++)
+    {
+    }
+
+    return 0;
+}
+
