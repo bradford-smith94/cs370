e393a6459f5caf68209033150807551a58cbd94b
Bradford Smith <bsmith8@stevens.edu>
2016-02-29 21:17:20 -0500

    Assignment5: small performance improvements
    Still needs to run faster.

--- hackerRank/skyscrapers/skyscrapers.py
+++ hackerRank/skyscrapers/skyscrapers.py
@@ -3,17 +3,16 @@
 # 02/29/2016
 # "We pledge our honor that we have abided by the Stevens Honor System."
 
-def routesForward(skyscrapers):
+def routesForward(skyscrapers, size):
     count = 0
-    for i in range(0, len(skyscrapers)):
-        height = skyscrapers[i]
-        for j in range(i + 1, len(skyscrapers)):
-            if skyscrapers[j] == height:
-                count += 1
-            elif skyscrapers[j] > height:
+    for i in range(0, size):
+        for j in range(i + 1, size):
+            if skyscrapers[j] > skyscrapers[i]:
                 break
+            elif skyscrapers[j] == skyscrapers[i]:
+                count += 1
     return count
 
 numSkyscrapers = int(input())
 skyscrapers = list(map(int, input().split()))
-print(2*routesForward(skyscrapers))
+print(2*routesForward(skyscrapers, numSkyscrapers))
