aed4d27fad45953baaeb2f00f6374870d62c3899
Bradford Smith <bsmith8@stevens.edu>
2016-03-13 23:49:56 -0400

    Assignment6: made real 2D array

--- spoj/trip/trip.py
+++ spoj/trip/trip.py
@@ -1,23 +1,23 @@
 # Aidan Racaniello, Bradford Smith, and Nicholas Zubrycki
 # CS 370 Assignment 6 SPOJ #33 TRIP
 # 03/13/2016
-# We pledge our honor that we have abided by the Stevens Honor System.
+# "We pledge our honor that we have abided by the Stevens Honor System."
 
 def lcs(x, y):
     n = len(x)
     m = len(y)
-    table = dict()  # a hashtable, but we'll use it as a 2D array here
+    table = [[0 for x in range(81)] for x in range(81)]
 
     for i in range(n+1):     # i=0,1,...,n
         for j in range(m+1):  # j=0,1,...,m
             if i == 0 or j == 0:
-                table[i, j] = 0
+                table[i][j] = 0
             elif x[i-1] == y[j-1]:
-                table[i, j] = table[i-1, j-1] + 1
+                table[i][j] = table[i-1][j-1] + 1
             else:
-                table[i, j] = max(table[i-1, j], table[i, j-1])
+                table[i][j] = max(table[i-1][j], table[i][j-1])
 
-    # Now, table[n, m] is the length of LCS of x and y.
+    # Now, table[n][m] is the length of LCS of x and y.
 
     # Let's go one step further and reconstruct
     # the actual sequence from DP table:
@@ -27,7 +27,7 @@ def lcs(x, y):
             return []
         elif x[i-1] == y[j-1]:
             return recon(i-1, j-1) + [x[i-1]]
-        elif table[i-1, j] > table[i, j-1]: #index out of bounds bug here: what if the first elements in the sequences aren't equal
+        elif table[i-1][j] > table[i][j-1]: #index out of bounds bug here: what if the first elements in the sequences aren't equal
             return recon(i-1, j)
         else:
             return recon(i, j-1)
